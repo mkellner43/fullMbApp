9c7d563a9636014090b2cb69b37b2096b6e2b3d3
mkellner43 <matthew.kellner@outlook.com>
2023-04-11 19:48:10 -0500

    commented out socket code

--- backend/app.js
+++ backend/app.js
@@ -14,53 +14,53 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 //socket io 
-const io = require('socket.io')(8900, {
-  cors: {
-    origin: process.env.FRONT_END_URL,
-  }
-});
-io.on('connection', async (socket) => {
-  socket.join(socket.handshake.auth.userID)
-  socket.on('send message', ({id, thread}) => {
-    socket.to(id).emit('message received', thread)
-  })
-  socket.on('typing', (id) => {
-    socket.to(id).emit('typing')
-    setTimeout(() => {
-      socket.to(id).emit('not typing')
-    }, 1000 * 50)
-  })
-  socket.on('not typing', (id) => {
-    socket.to(id).emit('not typing')
-  })
-  socket.on('notification', ({to_id, type, msg}) => {
-    socket.to(to_id).emit('notification', msg)
-  })
-  let users = [];
-  for (let [id, socket] of io.of("/").sockets) {
-    users.push(socket.handshake.auth.userID);
-  }
-  users = users.filter((value, index, array) => array.indexOf(value) === index)
+// const io = require('socket.io')(8900, {
+//   cors: {
+//     origin: process.env.FRONT_END_URL,
+//   }
+// });
+// io.on('connection', async (socket) => {
+//   socket.join(socket.handshake.auth.userID)
+//   socket.on('send message', ({id, thread}) => {
+//     socket.to(id).emit('message received', thread)
+//   })
+//   socket.on('typing', (id) => {
+//     socket.to(id).emit('typing')
+//     setTimeout(() => {
+//       socket.to(id).emit('not typing')
+//     }, 1000 * 50)
+//   })
+//   socket.on('not typing', (id) => {
+//     socket.to(id).emit('not typing')
+//   })
+//   socket.on('notification', ({to_id, type, msg}) => {
+//     socket.to(to_id).emit('notification', msg)
+//   })
+//   let users = [];
+//   for (let [id, socket] of io.of("/").sockets) {
+//     users.push(socket.handshake.auth.userID);
+//   }
+//   users = users.filter((value, index, array) => array.indexOf(value) === index)
 
-  io.emit('onlineFriends', users)
+//   io.emit('onlineFriends', users)
 
-  socket.on('logOut', (id) => {
-    users = users.filter(user => user !== id)
-    io.emit('onlineFriends', users)
-  })
+//   socket.on('logOut', (id) => {
+//     users = users.filter(user => user !== id)
+//     io.emit('onlineFriends', users)
+//   })
 
-  socket.on('reconnect', () => {
-    for (let [id, socket] of io.of("/").sockets) {
-      users.push(socket.handshake.auth.userID);
-    }
-    users = users.filter((value, index, array) => array.indexOf(value) === index)
-    io.emit('onlineFriends', users)
-  })
+//   socket.on('reconnect', () => {
+//     for (let [id, socket] of io.of("/").sockets) {
+//       users.push(socket.handshake.auth.userID);
+//     }
+//     users = users.filter((value, index, array) => array.indexOf(value) === index)
+//     io.emit('onlineFriends', users)
+//   })
 
-  socket.on('disconnect', () => {
-    const online = users.filter(user => user !== socket.handshake.auth.userID)
-    io.emit('onlineFriends', online) })
-})
+//   socket.on('disconnect', () => {
+//     const online = users.filter(user => user !== socket.handshake.auth.userID)
+//     io.emit('onlineFriends', online) })
+// })
 
 
 const indexRouter = require('./routes/index');
