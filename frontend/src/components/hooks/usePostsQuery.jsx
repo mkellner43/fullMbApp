f12e452221183b718232e8a30089cdef0a5086c8
mkellner43 <matthew.kellner@outlook.com>
2023-04-19 12:22:20 -0500

    added profile query hook

--- frontend/src/components/hooks/usePostsQuery.jsx
+++ frontend/src/components/hooks/usePostsQuery.jsx
@@ -1,9 +1,11 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { getPosts } from "../../api/posts";
+import { profile } from "../../api/user";
 import { setToken } from "../../pages/Login/features/loginSlice";
 import { useDispatch } from "react-redux";
 
-const usePostsQuery = () => {
+
+export const usePostsQuery = () => {
   const dispatch = useDispatch();
   return useInfiniteQuery({
     queryKey: ['posts'],
@@ -19,5 +21,22 @@ const usePostsQuery = () => {
     }
   })
 }
+
+export const useProfilePostsQuery = (id) => {
+  const dispatch = useDispatch();
+  return useInfiniteQuery({
+    queryKey: ['profile', id],
+    queryFn: async({pageParam=0}) => profile(id, pageParam),
+    getNextPageParam: (lastPage, pages) => {
+      if(lastPage.hasMore) return lastPage.cursor
+    },
+    retry: false,
+    enabled: !!id,
+    onError: (err) => {
+      document.cookie = 'access_token= ; max-age=0'
+      sessionStorage.clear()
+      dispatch(setToken())
+    }
+  })
+}
   
-export default usePostsQuery
